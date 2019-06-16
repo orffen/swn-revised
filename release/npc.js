f8b2deeab7c63ac58d10866547997bc86f8989d6
Orffen <orffen@outlook.com>
2019-06-16 17:35:06 +1000

    added release folder

---
+++ release/npc.js
@@ -0,0 +1,141 @@
+/**
+ * Copyright (c) 2019 Steve Simenic
+ *
+ * This file is part of the Stars Without Number Toolbox: Revised Edition
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+function generateBackground() {
+    var backgrounds = [
+        "The local underclass or poorest natives",
+        "Common laborers or cube workers",
+        "Aspiring burgeouise or upper class",
+        "The elite of this society"
+    ];
+    var result = "";
+    var roll = Math.floor((Math.random() * 6) + 1);
+    if (roll == 5) {
+        result = "Minority or foreigners - ";
+    }
+    else if (roll == 6) {
+        result = "Offworlders or exotics - ";
+    }
+    result += backgrounds[Math.floor(Math.random() * backgrounds.length)];
+    return result;
+}
+function generateRole() {
+    var roles = [
+        "Criminal, thug, thief, swindler",
+        "Menial, cleaner, retail worker, servant",
+        "Unskilled heavy leabor, porter, construction",
+        "Skilled trade, electrician, mechanic, pilot",
+        "Idea worker, programmer, writer",
+        "Merchant, business owner, trader, banker",
+        "Official, bureaucrat, courtier, clerk",
+        "Military, soldier, enforcer, law officer"
+    ];
+    return roles[Math.floor(Math.random() * roles.length)];
+}
+function generateProblem() {
+    var problems = [
+        "They have significant debt or money woes",
+        "A loved one is in trouble - ",
+        "Romantic failure with a desired person",
+        "Drug or behavioral addiction",
+        "Their superior dislikes or resents them",
+        "They have a persistent sickness",
+        "They hate their job or life situation",
+        "Someone dangerous is targeting them",
+        "They're pursuing a disastrous purpose",
+        "They have no problems worth mentioning"
+    ];
+    var result = "";
+    var roll = Math.floor((Math.random() * problems.length));
+    if (roll == 1) {
+        result = problems[roll];
+        while (roll == 1) {
+            roll = Math.floor((Math.random() * problems.length));
+        }
+        result += problems[roll];
+    }
+    else {
+        result = problems[roll];
+    }
+    return result;
+}
+function generateAge() {
+    var ages = [
+        "Unusually young or old for their role",
+        "Young adult",
+        "Mature prime",
+        "Middle-aged or elderly"
+    ];
+    return ages[Math.floor(Math.random() * ages.length)];
+}
+function generateDesire() {
+    var desires = [
+        "They want a particular romantic partner",
+        "They want money for them or a loved one",
+        "They want a promotion in their job",
+        "They want answers about a past trauma",
+        "They want revenge on an enemy",
+        "They want to help a beleagured friend",
+        "They want an entirely different job",
+        "They want protection from an enemy",
+        "They want to leave their current life",
+        "They want fame and glory",
+        "They want power over those around them",
+        "They have everything they want from life"
+    ];
+    return desires[Math.floor(Math.random() * desires.length)];
+}
+function generateTrait() {
+    var traits = [
+        "Ambition",
+        "Avarice",
+        "Bitterness",
+        "Courage",
+        "Cowardice",
+        "Curiosity",
+        "Deceitfulness",
+        "Determination",
+        "Devotion to a cause",
+        "Filiality",
+        "Hatred",
+        "Honesty",
+        "Hopefulness",
+        "Love of a person",
+        "Nihilism",
+        "Paternalism",
+        "Pessimism",
+        "Protectiveness",
+        "Resentment",
+        "Shame"
+    ];
+    return traits[Math.floor(Math.random() * traits.length)];
+}
+function generateNPC() {
+    var background = generateBackground();
+    var role = generateRole();
+    var problem = generateProblem();
+    var age = generateAge();
+    var desire = generateDesire();
+    var trait = generateTrait();
+    return [background, role, problem, age, desire, trait];
+}
---
+++ release/urban-encounter.js
@@ -0,0 +1,122 @@
+/**
+ * Copyright (c) 2019 Steve Simenic
+ *
+ * This file is part of the Stars Without Number Toolbox: Revised Edition
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+function generateVenue() {
+    var venue = [
+        "In the middle of the street",
+        "In a public plaza",
+        "Down a side alley",
+        "Inside a local business",
+        "Next to or in a public park",
+        "At a mass-transit station"
+    ];
+    return venue[Math.floor(Math.random() * venue.length)];
+}
+function generateInvolvement() {
+    var involvement = [
+        "A sympathetic participant appeals to them",
+        "Ways around it are all dangerous/blocked",
+        "It happens immediately around them",
+        "A valuable thing looks snatchable amid it",
+        "A participant offers a reward for help",
+        "Someone mistakenly involves the PCs in it",
+        "The seeming way out just leads deeper in",
+        "Responsibility is somehow pinned on them"
+    ];
+    return involvement[Math.floor(Math.random() * involvement.length)];
+}
+function generateNature() {
+    var natures = [
+        "A parade or festival is being disrupted",
+        "Innocents are being assaulted",
+        "An establishment is being robbed",
+        "A disturbance over local politics happens",
+        "Someone is being blamed for something",
+        "Fires or building collapses are happening",
+        "A medical emergency is happening",
+        "Someone's trying to cheat the PCs",
+        "A vehicle accident is happening",
+        "A religious ceremony is being disrupted"
+    ];
+    return natures[Math.floor(Math.random() * natures.length)];
+}
+function generateReason() {
+    var reasons = [
+        "Money, extortion, payment due, debts",
+        "Respect, submission to social authority",
+        "Grudges, ethnic resentment, gang payback",
+        "Politics, religion, or other ideology"
+    ];
+    return reasons[Math.floor(Math.random() * reasons.length)];
+}
+function generateAntagonists() {
+    var antagonists = [
+        "A local bully and their thugs",
+        "A ruthless political boss and their zealots",
+        "Violent criminals",
+        "Religious fanatics",
+        "A blisteringly obnoxious offworlder",
+        "Corrupt or over-strict government official",
+        "A mob of intoxicated locals",
+        "A ranting demagogue and their followers",
+        "A stupidly bull-headed local grandee",
+        "A very capable assassin or strong-arm",
+        "A self-centered local scion of power",
+        "A confused foreigner or backwoodsman"
+    ];
+    return antagonists[Math.floor(Math.random() * antagonists.length)];
+}
+function generateFeatures() {
+    var features = [
+        "Heavy traffic running through the place",
+        "Music blaring at deafening volumes",
+        "Two groups present that detest each other",
+        "Large delivery taking place right there",
+        "Swarm of schoolkids or feral youth",
+        "Insistent soapbox preacher here",
+        "Several pickpockets working the crowd",
+        "A kiosk is tipping over and spilling things",
+        "Streetlights are out or visibility is low",
+        "A cop patrol is here and reluctant to act",
+        "PC-hostile reporters are recording here",
+        "Someone's trying to sell something to PCs",
+        "Feral dogs or other animals crowd here",
+        "Unrelated activists are protesting here",
+        "Street kids are trying to steal from the PCs",
+        "GPS maps are dangerously wrong here",
+        "Downed power lines are a danger here",
+        "Numerous open manholes and utility holes",
+        "The street's blockaded by something",
+        "Crowds so thick one can barely move"
+    ];
+    return features[Math.floor(Math.random() * features.length)];
+}
+function generateUrbanEncounter() {
+    var venue = generateVenue();
+    var involvement = generateInvolvement();
+    var nature = generateNature();
+    var reason = generateReason();
+    var antagonists = generateAntagonists();
+    var features = generateFeatures();
+    return [venue, involvement, nature, reason, antagonists, features];
+}
